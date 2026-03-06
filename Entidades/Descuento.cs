ccf4aaa1c3387ebb7da86cf227bdbbd80598356b
yasruffi <ruffinengoyasmin@gmail.com>
2026-03-06 16:45:12 -0300

    RepositorioDescuento y cositas de la clase Descuento

--- Entidades/Descuento.cs
+++ Entidades/Descuento.cs
@@ -5,18 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Entidades
- {   
+ {
     public enum TipoDescuento
     {
-        Fijo,
-        Dinamico
+        MontoFijo,
+        Porcentaje
     }
 
     public class Descuento
     {
         public int DescuentoId { get; set; }
         public string Nombre { get; set; }
-        public string Monto { get; set; }
+        public int Valor { get; set; } 
         public bool Activo { get; set; }
         public TipoDescuento TipoDeDescuento { get; set; } //enum para porcentaje o un monto
         public TipoCliente TipoCliente { get; set; }    
